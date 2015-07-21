3c9a107e633558d50f4adb9b3d379e0054c974b2
DL014945 <daniel.lloyd@cerner.com>
2015-07-21 09:50:04 -0500

    Removed unnecessary login in attribute file

--- attributes/customized-multipath.rb
+++ attributes/customized-multipath.rb
@@ -1,4 +1,2 @@
-if node['multipath']['storage_type'] == "customized"
-  default['multipath']['defaults']['user_friendly_names'] = "yes"
-  default['multipath']['defaults']['find_multipaths'] =  "yes"
-end
+default['multipath']['defaults']['user_friendly_names'] = "yes"
+default['multipath']['defaults']['find_multipaths'] =  "yes"
